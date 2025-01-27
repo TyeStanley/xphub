2619a458ae993da6ba7c2e037bbef236ad6dd551
tstanley090 <tyestanley090@gmail.com>
2025-01-27 13:17:45 -0800

    partially done hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,31 @@
+import { Button } from '@/components/ui/button';
+
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-center bg-gray-50">
-      <h1>XPHUB</h1>
+    <main className="flex flex-col items-center justify-center">
+      {/* Hero Section */}
+      <section id="hero" className="flex flex-col bg-[#F7F7FD] py-8 md:flex-row">
+        <div className="flex flex-col gap-4 px-4">
+          <p className="text-center text-sm font-bold uppercase text-primary">
+            Transparency on every load, Trust in every turn
+          </p>
+          <h1 className="text-center text-4xl font-bold text-[#000929]">
+            Trusted Liquid Bulk Transport Solutions
+          </h1>
+          <p className="text-center text-sm text-[#4D5461]">
+            We’re all about being clear with every load & ensuring your liquid reaches its
+            destination safely and on time.
+          </p>
+
+          {/* Get a quote button */}
+          <Button className="mt-3 w-full bg-primary text-white">
+            {/* TODO: Create Modal for quote form */}
+            Get a quote
+          </Button>
+        </div>
+        <div className="flex-shrink-0">image</div>
+      </section>
+
       <section className="pb-[500px]" id="services">
         <h2>Services</h2>
       </section>
