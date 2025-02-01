d71f988d2a38049cd19b12d0d47e04cbeb887e4d
tstanley090 <tyestanley090@gmail.com>
2025-02-01 14:46:48 -0800

    added confirmation for user after submitting form

--- src/components/QuoteModal.tsx
+++ src/components/QuoteModal.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import {
   Dialog,
   Transition,
@@ -6,7 +8,7 @@ import {
   TransitionChild,
   Button,
 } from '@headlessui/react';
-import { Fragment } from 'react';
+import { Fragment, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { X } from 'lucide-react';
 import { formFields } from '@/constants';
@@ -30,6 +32,7 @@ interface QuoteModalProps {
 }
 
 export default function QuoteModal({ isOpen, onClose }: QuoteModalProps) {
+  const [isSuccess, setIsSuccess] = useState(false);
   const {
     register,
     handleSubmit,
@@ -39,10 +42,12 @@ export default function QuoteModal({ isOpen, onClose }: QuoteModalProps) {
 
   const onSubmit = async (data: QuoteFormData) => {
     try {
-      const result = await sendQuoteRequest(data);
+      console.log(data);
+      // const result = await sendQuoteRequest(data);
+      const result = { success: true };
       if (result.success) {
         reset();
-        onClose();
+        setIsSuccess(true);
       } else {
         console.error('Failed to send quote request');
       }
@@ -63,7 +68,10 @@ export default function QuoteModal({ isOpen, onClose }: QuoteModalProps) {
           leaveFrom="opacity-100"
           leaveTo="opacity-0"
         >
-          <div className="fixed bottom-0 left-0 right-0 top-[65px] bg-black/30 backdrop-blur-sm" />
+          <div
+            onClick={() => setIsSuccess(false)}
+            className="fixed bottom-0 left-0 right-0 top-[65px] bg-black/30 backdrop-blur-sm"
+          />
         </TransitionChild>
 
         <div className="fixed bottom-0 left-0 right-0 top-[65px] overflow-y-auto">
@@ -95,62 +103,84 @@ export default function QuoteModal({ isOpen, onClose }: QuoteModalProps) {
                     <X className="h-5 w-5 text-gray-900" />
                   </button>
                 </div>
-
-                <form onSubmit={handleSubmit(onSubmit)} className="mt-5 space-y-5">
-                  {formFields.map((row, rowIndex) => (
-                    <div key={rowIndex} className="space-y-4 md:flex md:gap-5 md:space-y-0">
-                      {row.map((field) => (
-                        <div key={field.name} className="w-full">
-                          <label
-                            htmlFor={field.name}
-                            className="mb-2 block text-sm text-[#344054] md:text-base"
-                          >
-                            {field.label}
-                          </label>
-                          <input
-                            {...register(field.name, {
-                              required: field.required,
-                              ...(field.pattern && { pattern: field.pattern }),
-                            })}
-                            type={field.type}
-                            className="w-full rounded-lg border border-gray-300 p-3 text-[#101828] caret-primary placeholder:text-gray-500 focus:border-primary focus:shadow-[0px_0px_0px_3px_rgba(167,43,42,0.10)] focus:outline-none focus:ring-1 focus:ring-primary"
-                            placeholder={field.placeholder}
-                          />
-                          {errors[field.name] && (
-                            <p className="mt-1 text-sm text-red-600">
-                              {errors[field.name]?.message}
-                            </p>
-                          )}
-
-                          {field.note && (
-                            <p className="mt-1 text-sm text-[#DC6803]">{field.note}</p>
-                          )}
-                        </div>
-                      ))}
+                {isSuccess ? (
+                  <div className="flex flex-col items-center justify-center gap-5 pt-5 text-center">
+                    <div className="relative h-[64px] w-[64px] md:h-[110px] md:w-[110px]">
+                      <Image src="/assets/success-modal-icon.png" alt="" width={110} height={110} />
                     </div>
-                  ))}
+                    <h3 className="text-xl font-bold text-[#101828]">Quote Request Submitted!</h3>
 
-                  <div>
-                    <label
-                      htmlFor="transportDetails"
-                      className="mb-2 block text-sm text-[#344054] md:text-base"
+                    <p className="text-sm text-[#475467] md:w-[500px] md:text-base">
+                      We&apos;ll be in touch within 48 hours with your personalized quote, sent to
+                      the email or phone number you provided.
+                    </p>
+                    <Button
+                      onClick={() => {
+                        onClose();
+                        setIsSuccess(false);
+                      }}
+                      className="w-full rounded-lg bg-primary py-3 text-white hover:bg-[#991818]"
                     >
-                      Transport Details
-                    </label>
-                    <textarea
-                      {...register('transportDetails')}
-                      className="h-24 w-full rounded-lg border border-gray-300 p-3 text-[#101828] caret-primary placeholder:text-gray-500 focus:border-primary focus:shadow-[0px_0px_0px_3px_rgba(167,43,42,0.10)] focus:outline-none focus:ring-1 focus:ring-primary"
-                      placeholder="Provide any specific transport requirements or instructions"
-                    />
+                      Got It, Thanks!
+                    </Button>
                   </div>
+                ) : (
+                  <form onSubmit={handleSubmit(onSubmit)} className="mt-5 space-y-5">
+                    {formFields.map((row, rowIndex) => (
+                      <div key={rowIndex} className="space-y-4 md:flex md:gap-5 md:space-y-0">
+                        {row.map((field) => (
+                          <div key={field.name} className="w-full">
+                            <label
+                              htmlFor={field.name}
+                              className="mb-2 block text-sm text-[#344054] md:text-base"
+                            >
+                              {field.label}
+                            </label>
+                            <input
+                              {...register(field.name, {
+                                required: field.required,
+                                ...(field.pattern && { pattern: field.pattern }),
+                              })}
+                              type={field.type}
+                              className="w-full rounded-lg border border-gray-300 p-3 text-[#101828] caret-primary placeholder:text-gray-500 focus:border-primary focus:shadow-[0px_0px_0px_3px_rgba(167,43,42,0.10)] focus:outline-none focus:ring-1 focus:ring-primary"
+                              placeholder={field.placeholder}
+                            />
+                            {errors[field.name] && (
+                              <p className="mt-1 text-sm text-red-600">
+                                {errors[field.name]?.message}
+                              </p>
+                            )}
 
-                  <Button
-                    type="submit"
-                    className="w-full rounded-lg bg-primary py-3 text-white hover:bg-[#991818]"
-                  >
-                    Get a quote
-                  </Button>
-                </form>
+                            {field.note && (
+                              <p className="mt-1 text-sm text-[#DC6803]">{field.note}</p>
+                            )}
+                          </div>
+                        ))}
+                      </div>
+                    ))}
+
+                    <div>
+                      <label
+                        htmlFor="transportDetails"
+                        className="mb-2 block text-sm text-[#344054] md:text-base"
+                      >
+                        Transport Details
+                      </label>
+                      <textarea
+                        {...register('transportDetails')}
+                        className="h-24 w-full rounded-lg border border-gray-300 p-3 text-[#101828] caret-primary placeholder:text-gray-500 focus:border-primary focus:shadow-[0px_0px_0px_3px_rgba(167,43,42,0.10)] focus:outline-none focus:ring-1 focus:ring-primary"
+                        placeholder="Provide any specific transport requirements or instructions"
+                      />
+                    </div>
+
+                    <Button
+                      type="submit"
+                      className="w-full rounded-lg bg-primary py-3 text-white hover:bg-[#991818]"
+                    >
+                      Get a quote
+                    </Button>
+                  </form>
+                )}
               </DialogPanel>
             </TransitionChild>
           </div>
